f3a4d59d4391179e835b42e0f31f895c3abed432
Andy Wu <49007969+sudogene@users.noreply.github.com>
2021-01-24 11:25:07 +0800

    Add files via upload

---
+++ example.py
@@ -0,0 +1,30 @@
+import telegrapy
+import time
+
+# Obtain token and create bot. See https://core.telegram.org/bots#6-botfather
+token = 'YOUR_TOKEN_HERE'
+bot = telegrapy.Bot(token)
+
+# Get handler of bot, for adding of handler functions
+handler = bot.handler
+
+
+# All handler functions takes in one argument: Message
+def echo(msg: telegrapy.Message):
+    chat_id = msg.chat_id
+    text = msg.text
+    bot.send_message(chat_id, text)
+
+
+# Add defined handler function to the bot with specified command word
+handler.add_command('echo', echo)
+
+
+def main():
+    bot.run()
+    print('Running...')
+    while True:
+        time.sleep(3)
+
+
+main()
